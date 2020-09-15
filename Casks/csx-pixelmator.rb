04a9d623efca9669cde2dc2ed2a9f0b40283fde9
chaoskyx <chaosky.me@gmail.com>
2020-09-15 11:54:25 +0800

    Add apps

--- Casks/csx-pixelmator.rb
+++
@@ -1,9 +0,0 @@
-cask "csx-pixelmator" do
-    version :latest
-    sha256 :no_check
-  
-    url "https://github.com/chaoskyx/homebrew-brew/raw/apps/Pixelmator_3.9.dmg"
-    name "Pixelmator"
-  
-    app "Pixelmator.app"
-end
--- Casks/csx-popclip.rb
+++
@@ -1,9 +0,0 @@
-cask "csx-popclip" do
-    version :latest
-    sha256 :no_check
-  
-    url "https://github.com/chaoskyx/homebrew-brew/raw/apps/PopClip%202019.10.dmg"
-    name "PopClip"
-  
-    app "PopClip.app"
-end
