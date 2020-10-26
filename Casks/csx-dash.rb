d8fc8c17dea29ccd336a223daf3dfecce6d38dd3
chaosky <chaosky.me@gmail.com>
2020-10-26 09:26:31 +0800

    update Dash 5.5.0

--- Casks/csx-dash.rb
+++ Casks/csx-dash.rb
@@ -2,7 +2,7 @@ cask "csx-dash" do
     version :latest
     sha256 :no_check
   
-    url "https://github.com/chaoskyx/homebrew-brew/raw/apps/Dash_5.4.0.dmg"
+    url "https://github.com/chaoskyx/homebrew-brew/raw/apps/Dash_5.5.0.dmg"
     name "Dash"
   
     preflight do
